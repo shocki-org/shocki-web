23d42f4f47cf1308ad6914fa792275a5af2ec428
PleBea <bini060315kim@gmail.com>
2024-09-14 11:03:12 +0900

    feat(input): Implement input component

---
+++ src/components/Input/Input.tsx
@@ -0,0 +1,46 @@
+import { useCallback, useReducer } from "react";
+import MaterialIcon from "../MaterialIcon/MaterialIcon";
+import styles from "./style.module.scss";
+
+export interface Props {
+  label: string;
+  placeholder?: string;
+  visibility?: boolean;
+  state: [string, (value: string) => void];
+}
+
+const Input = (props: Props) => {
+  const [value, setValue] = props.state;
+  const [visibility, toggleVisibility] = useReducer((prev) => !prev, false);
+
+  const handleChange = useCallback(
+    (e: React.ChangeEvent<HTMLInputElement>) => {
+      setValue(e.target.value);
+    },
+    [setValue]
+  );
+
+  return (
+    <div className={styles.inputContainer}>
+      <p>{props.label}</p>
+
+      <div className={styles.inputWrapper}>
+        <input
+          placeholder={props.placeholder}
+          value={value}
+          onChange={handleChange}
+        />
+
+        {props.visibility && (
+          <button onClick={toggleVisibility}>
+            <MaterialIcon>
+              {visibility ? "visibility_off" : "visibility"}
+            </MaterialIcon>
+          </button>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Input;
