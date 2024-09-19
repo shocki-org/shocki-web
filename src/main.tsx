697e302b62d1556d18430f92d340da93c3cfc025
PleBea <bini060315kim@gmail.com>
2024-09-19 10:11:52 +0900

    feat(provider): Apply QueryProvider component

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,14 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import RouterProvider from "./shared/providers/RouterProvider";
 import FontProvider from "./shared/providers/FontProvider";
+import QueryProvider from "./shared/providers/QueryProvider";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <FontProvider>
-      <RouterProvider />
-    </FontProvider>
+    <QueryProvider>
+      <FontProvider>
+        <RouterProvider />
+      </FontProvider>
+    </QueryProvider>
   </StrictMode>
 );
