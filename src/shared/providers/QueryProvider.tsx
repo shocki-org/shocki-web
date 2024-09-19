022c88da7c65737ff067071bd3d8f4476f71810c
PleBea <bini060315kim@gmail.com>
2024-09-19 10:11:29 +0900

    feat(provider): Add QueryProvider component for managing queries

---
+++ src/shared/providers/QueryProvider.tsx
@@ -0,0 +1,14 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { PropsWithChildren } from "react";
+
+const queryClient = new QueryClient();
+
+const QueryProvider = (props: PropsWithChildren) => {
+  return (
+    <QueryClientProvider client={queryClient}>
+      {props.children}
+    </QueryClientProvider>
+  );
+};
+
+export default QueryProvider;
